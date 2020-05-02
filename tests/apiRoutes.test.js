2a623e6fe571d810c0da26688b2812dadadeb52c
kwu3 <kwu3@villanova.edu>
2020-05-02 17:04:42 -0400

    Added tests for get requests on htmlroutes and apiroutes

---
+++ tests/apiRoutes.test.js
@@ -0,0 +1,49 @@
+const axios = require("axios");
+
+describe("API Routes", () => {
+	describe("get request on /api/notes", () => {
+		it("should return a status code of 200", async () => {
+			const { status } = await axios.get("/api/notes");
+			expect(status).toBe(200);
+		});
+		it("should have content type json", async () => {
+			const expectedContentType = "application/json";
+			const { headers } = await axios.get("/api/notes");
+
+			const hasJSON =
+				JSON.stringify(headers).indexOf(expectedContentType) !== -1;
+
+			expect(hasJSON).toBe(true);
+		});
+		it("should return an array", async () => {
+			const { data } = await axios.get("/api/notes");
+			const isArray = Array.isArray(data);
+			expect(isArray).toBeTruthy();
+			expect(data).toBeTruthy();
+		});
+		it("it should return an array of objects if it's not empty", async () => {
+			const { data } = await axios.get("/api/notes");
+			const isObject = typeof data[0] === "object";
+
+			if (data.length > 0) {
+				expect(isObject).toBe(true);
+			} else {
+				expect(isObject).toBe(false);
+			}
+		});
+		it("should have the correct properties (title, text, id)", async () => {
+			const { data } = await axios.get("/api/notes");
+			const isObject = typeof data[0] === "object";
+
+			if (data.length > 0) {
+				const [note] = data;
+				const { title, text, id } = note;
+				expect(title).toBeTruthy();
+				expect(text).toBeTruthy();
+				expect(id).toBeTruthy();
+			} else {
+				expect(isObject).toBe(false);
+			}
+		});
+	});
+});
