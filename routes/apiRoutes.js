e214ad2e209e18db777fa404ce6a26a7669e58b2
kwu3 <kwu3@villanova.edu>
2020-05-02 15:33:47 -0400

    Added comments

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,11 +4,11 @@ const fs = require("fs");
 const DbFunctions = require("../db/dbFunctions.js");
 
 const dbFunctions = new DbFunctions();
-// const { getNotes, addNote, deleteNote } = dbFunctions;
 
 // GET
 router.get("/notes", async (req, res) => {
 	try {
+		// get the notes, then return them to the user client
 		const notesJSON = await dbFunctions.getNotes();
 		res.json(notesJSON);
 	} catch (err) {
@@ -18,13 +18,17 @@ router.get("/notes", async (req, res) => {
 
 // POST
 router.post("/notes", (req, res) => {
+	// get the body of the user post request
 	const newNote = req.body;
+	// add it to the notes in db.json
 	dbFunctions.addNote(newNote);
+	// return the new note that the user added
 	res.json(newNote);
 });
 
 // DELETE
 router.delete("/notes/:id", (req, res) => {
+	// delete the note with the same id
 	res.json(dbFunctions.deleteNote(req.params.id));
 });
 
